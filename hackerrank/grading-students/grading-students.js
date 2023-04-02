69b505117e38807c8e319a7b0316543392debbc4
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-02 15:20:43 +0300

    update

--- hackerrank/grading-students/grading-students.js
+++
@@ -1,9 +0,0 @@
-function gradingStudents(grades) {
-  for (let i in grades) {
-    if (grades[i] >= 38) {
-      const x = grades[i] % 5;
-      if (x >= 3) grades[i] += 5 - x;
-    }
-  }
-  return grades;
-}
